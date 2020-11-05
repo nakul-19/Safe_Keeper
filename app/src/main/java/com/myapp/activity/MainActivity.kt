a0986f216444c48c32957f2cec98079f2a88be07
Nakul Sharma <rnqnkl@gmail.com>
2020-11-05 22:01:42 +0530

    Added drawer.

--- app/src/main/java/com/myapp/activity/MainActivity.kt
+++ app/src/main/java/com/myapp/activity/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.myapp.activity
 
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.myapp.R
 import com.myapp.fragments.HomeFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawer = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val toggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        drawer.addDrawerListener(toggle)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeButtonEnabled(true)
+        toggle.syncState()
         val arg = Bundle()
         val name = intent.getStringExtra("Name")
         arg.putString("Name", name)
@@ -18,4 +31,13 @@ class MainActivity : AppCompatActivity() {
         frag.arguments = arg
         supportFragmentManager.beginTransaction().add(R.id.main_frame, frag).commit()
     }
+
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else
+            super.onBackPressed()
+    }
+
 }
