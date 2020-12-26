27c48e2bec30b1a1b62049ca0e2e88a0b6d33a14
Nakul Sharma <rnqnkl@gmail.com>
2020-12-26 23:24:04 +0530

    Code fixes.

--- app/src/main/java/com/myapp/fragments/SignUpFragment.kt
+++ app/src/main/java/com/myapp/fragments/SignUpFragment.kt
@@ -2,11 +2,13 @@ package com.myapp.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -28,17 +30,17 @@ class SignUpFragment : BaseFragment() {
     lateinit var login2: TextView
     lateinit var adapter: AvatarAdapter
     lateinit var loading: ProgressBar
-    val list: ArrayList<Int> = ArrayList()
+    private val list: ArrayList<Int> = ArrayList()
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_sign_up, container, false)
         val recycler = view.findViewById<RecyclerView>(R.id.avatarRecycler)
         recycler.layoutManager= LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+
         list.add(R.drawable.avatar1)
         list.add(R.drawable.avatar3)
         list.add(R.drawable.avatar2)
@@ -47,9 +49,12 @@ class SignUpFragment : BaseFragment() {
         list.add(R.drawable.avatar6)
         list.add(R.drawable.avatar7)
         list.add(R.drawable.avatar8)
+
         adapter= AvatarAdapter(list)
         recycler.adapter=adapter
+
         init(view)
+
         signup.setOnClickListener {
 
             loading.visibility = View.VISIBLE
@@ -64,6 +69,7 @@ class SignUpFragment : BaseFragment() {
             MainScope().launch {
                 context?.let {
                     user = UserDatabase(it).getUserDao().getUser(email.text.toString())
+
                     if (user == null) {
                         UserDatabase(it).getUserDao().addUser(
                             User(
@@ -78,6 +84,7 @@ class SignUpFragment : BaseFragment() {
                         launchMainActivity(user!!)
                     } else
                         email.error = "Username already in use!"
+
                     loading.visibility = View.GONE
                 }
             }
@@ -85,32 +92,37 @@ class SignUpFragment : BaseFragment() {
         }
 
         login.setOnClickListener {
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.login_frame, SignInFragment()).commit()
-            requireActivity().findViewById<TextView>(R.id.signUp).setTextColor(resources.getColor(R.color.colorInactive))
-            requireActivity().findViewById<TextView>(R.id.signIn).setTextColor(resources.getColor(R.color.white))
-            val ml = requireActivity().findViewById<MotionLayout>(R.id.motionLayout)
-            ml.transitionToStart()
+            openSignIn()
         }
+
         login2.setOnClickListener {
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.login_frame, SignInFragment()).commit()
-            requireActivity().findViewById<TextView>(R.id.signUp).setTextColor(resources.getColor(R.color.colorInactive))
-            requireActivity().findViewById<TextView>(R.id.signIn).setTextColor(resources.getColor(R.color.white))
-            val ml = requireActivity().findViewById<MotionLayout>(R.id.motionLayout)
-            ml.transitionToStart()
+            openSignIn()
         }
+
         return view
+    }
+
+    private fun openSignIn(){
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.login_frame, SignInFragment()).commit()
+        requireActivity().findViewById<TextView>(R.id.signUp).setTextColor(resources.getColor(R.color.colorInactive))
+        requireActivity().findViewById<TextView>(R.id.signIn).setTextColor(resources.getColor(R.color.white))
+        val ml = requireActivity().findViewById<MotionLayout>(R.id.motionLayout)
+        ml.transitionToStart()
     }
 
     private fun launchMainActivity(user: User) {
         Toast.makeText(requireContext(), "Welcome ${email.text}!", Toast.LENGTH_SHORT).show()
+
         val intent = Intent(requireActivity(), MainActivity::class.java)
+
         intent.putExtra("Name", name.text.toString())
         intent.putExtra("Email", email.text.toString())
         intent.putExtra("Uid", user.userId)
         intent.putExtra("Avatar", adapter.getSelected())
+
         startActivity(intent)
+
         requireActivity().overridePendingTransition(R.anim.slide_in, R.anim.slide_out)
         requireActivity().finish()
     }
